4cd1d8378aeb330cc61263429d13f5a2023a7ca8
Jan Tore Morken <jantore@32k.org>
2018-05-07 21:30:39 +0200

    Release v0.0.2

--- README.md
+++ README.md
@@ -28,7 +28,7 @@ Jan Tore Morken <dist@jantore.net>
 
 # COPYRIGHT AND LICENSE
 
-This software is copyright (c) 2014 by Jan Tore Morken.
+This software is copyright (c) 2014-2018 by Jan Tore Morken.
 
 This is free software; you can redistribute it and/or modify it under
 the same terms as the Perl 5 programming language system itself.
--- lib/Mojolicious/Plugin/Myriad.pm
+++ lib/Mojolicious/Plugin/Myriad.pm
@@ -1,7 +1,7 @@
 package Mojolicious::Plugin::Myriad;
 
 use base 'Mojolicious::Plugin';
-use version; our $VERSION = version->declare("v0.0.1");
+use version; our $VERSION = version->declare("v0.0.2");
 
 use Myriad::Schema;
 
@@ -83,7 +83,7 @@ Jan Tore Morken <dist@jantore.net>
 
 =head1 COPYRIGHT AND LICENSE
 
-This software is copyright (c) 2014 by Jan Tore Morken.
+This software is copyright (c) 2014-2018 by Jan Tore Morken.
 
 This is free software; you can redistribute it and/or modify it under
 the same terms as the Perl 5 programming language system itself.
